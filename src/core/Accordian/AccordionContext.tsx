b6db3eb49d22f78a5e10cc2ad880cce5e05f0aec
Darron Brown <darronbrown88@outlook.com>
2023-03-09 06:28:27 -0700

    Update AccordionContext.tsx

--- src/core/Accordian/AccordionContext.tsx
+++ src/core/Accordian/AccordionContext.tsx
@@ -1,43 +1,14 @@
-import React, { FunctionComponent, ReactNode, useCallback } from 'react';
-import { Pressable, ViewProps } from 'react-native';
-import Animated, { AnimateProps, Layout, useAnimatedStyle, useDerivedValue, withSpring } from 'react-native-reanimated';
-import styled from 'styled-components/native';
+import { createContext, useContext } from 'react';
 
-import { useAccordion } from './AccordionContext';
-
-type Props = {
-    icon?: ReactNode;
-} & AnimateProps<ViewProps>;
-
-const AccordionButton: FunctionComponent<Props> = ({ icon, children, ...rest }) => {
-    const { isExpanded, onToggle } = useAccordion();
-
-    const iconRotation = useDerivedValue(() => withSpring(isExpanded ? 180 : 0, { overshootClamping: true }));
-    const rStyle = useAnimatedStyle(() => ({ transform: [{ rotate: `${iconRotation.value}deg` }] }));
-
-    const handlePress = useCallback(onToggle, [onToggle]);
-
-    return (
-        <Animated.View layout={Layout} {...rest}>
-            <StyledPressable accessibilityRole="button" onPress={handlePress}>
-                {children}
-
-                {!!icon && <IconContainer style={rStyle}>{icon}</IconContainer>}
-            </StyledPressable>
-        </Animated.View>
-    );
+type AccordionContextType = {
+    isExpanded: boolean;
+    onToggle(): void;
 };
 
-const StyledPressable = styled(Pressable)`
-    align-items: center;
-    justify-content: flex-start;
-    flex-direction: row;
-`;
+const defaultAccordionContext: AccordionContextType = {
+    isExpanded: false,
+    onToggle: () => {},
+};
 
-const IconContainer = styled(Animated.View)`
-    align-items: center;
-    justify-content: center;
-    margin-left: auto;
-`;
-
-export default AccordionButton;
+export const AccordionContext = createContext<AccordionContextType>(defaultAccordionContext);
+export const useAccordion = () => useContext(AccordionContext);
