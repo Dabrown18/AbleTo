6b754746d6f7f8d87e152ce0f27dd33e2f0d3d7a
Darron Brown <dabrown18@live.com>
2023-03-08 20:57:40 -0700

    fix linting

---
+++ src/core/Text/BodyText.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BodyText = () => {
+  return (
+    <View>
+      <Text>BodyText</Text>
+    </View>
+  );
+};
+
+export default BodyText;
